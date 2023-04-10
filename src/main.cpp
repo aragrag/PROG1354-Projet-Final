322529ee7a65d7415d1fddf99941684dbb5bb64b
Ayoub <aragrag.ayoub@gmail.com>
2023-04-10 18:24:19 -0300

    Nettoyage main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,12 @@
 #include <Adafruit_BME280.h>
 #include <AsyncElegantOTA.h>
 
-
 #define TIMELOOP 15000 // Temps de traitement 3 sec
-#define SEALEVELPRESSURE_HPA (1013.25)
-#define sensorPin  33 //Broche capteur de lumiere
-#define ledPin 13 //Broche LED
-#define pinWind 4 //Broche du capteur de vent 
-#define sensorPluie 32 //Broche du capteur de pluie
 
 //Declaration des constantes
 
 const float PERIOD_MES = TIMELOOP / 1000;
 const float MILE_TO_KILO = 1.60934;
-
-//Declaration des variables
-
-unsigned long presentMillis = 0 ;
-
-
 
 const char* ssid = "lathytharma";
 const char* password = "theking5";
@@ -33,9 +21,10 @@ const char* password = "theking5";
 // const char* ssid = "UNIFI_IDO1";
 // const char* password = "42Bidules!";
 
-
 const char* FILE_NAME = "/data.json"; // Dossier des donnees 
 
+//Declaration des variables
+unsigned long presentMillis = 0 ;
 
 
 
@@ -55,7 +44,6 @@ void setup() {
 	}
 
   // Se connecter au WiFi vec (ssid, password) 
-
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {   // Wait for connection
     delay(500);
@@ -70,13 +58,11 @@ void setup() {
 
 
   // Configration de time Zone et rechuprer la date et heure courant
-
   configTime(0, 0, "ca.pool.ntp.org", "time.nist.gov");
   time_t timestamp = time(NULL);
 
   
   // Vérification  de la Biblio SPIFFS
-
   if(!SPIFFS.begin(true)) {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
@@ -105,11 +91,6 @@ void setup() {
 
 
 void loop() {
-// ---------------------------------------
-// ------- TRAINTEMENT DES DONNEES -------
-// ---------------------------------------
-
-
 
   // Obtenir le temps actuel en timestamp
   presentMillis = millis();
@@ -130,23 +111,16 @@ void loop() {
   }
 
   // Parse le contenu du fichier JSON en un tableau d'objets JSON
-
   DynamicJsonDocument doc(1024);
-  //deserializeJson(doc, file);
 
   // Ajoute un nouvel objet JSON au tableau
-
   JsonObject obj = doc.createNestedObject();
   obj["date"] = timestamp; // Ajouter la date
   obj["temperature"] = round(bme.readTemperature() * 100.0) / 100.0; // Ajouter la temperature
   obj["pressure"] = round((bme.readPressure() / 100.0F) * 100.0) / 100.0; // Ajouter le pressure
   obj["humidity"] = round(bme.readHumidity() * 100.0) / 100.0; // Ajouter l'humidite
-  // obj["luminosity"] = round(sensorValue * 100.0) / 100.0; // Ajouter luminosite
-  // obj["wind"] = windSpeed; // Ajouter luminosite
-  // obj["rain"] = sensorValeurPluie; // Ajouter luminosite
 
   // Écrit le contenu du tableau dans le fichier JSON
-
   file.seek(0); 
   serializeJson(doc, file);
   file.close();
