3929fd73b724e96be8dd2da30b8468753c7727e7
Ayoub <aragrag.ayoub@gmail.com>
2023-04-10 13:23:53 -0300

    Edit .gitignore File -  add .pio

---
+++ .pio/build/projetgroupesp32dev/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/projetgroupesp32dev/FrameworkArduino/HWCDC.cpp.o: \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/projetgroupesp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/projetgroupesp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
