90cacc0499957b4202ee0958fe1dc3e8d13d377a
Ayoub <aragrag.ayoub@gmail.com>
2023-04-10 22:19:58 -0300

    Add .pio

---
+++ .pio/build/projetgroupesp32dev/FrameworkArduino/USBCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/projetgroupesp32dev/FrameworkArduino/USBCDC.cpp.o: \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/aragr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
