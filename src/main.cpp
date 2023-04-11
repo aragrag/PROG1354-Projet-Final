2fb5e1e9db64ec42d4c0db30faab8c7aee804e52
Ayoub <aragrag.ayoub@gmail.com>
2023-04-10 22:35:22 -0300

    Edit main.cpp (nettoyage)

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include <AsyncElegantOTA.h>
 
 #define TIMELOOP 15000 // Temps de traitement 3 sec
 
@@ -81,8 +80,7 @@ void setup() {
   // Servir les fichiers HTML, CSS et JS depuis le système de fichiers SPIFFS (index.html)
   server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
-  // Start ElegantOTA To update project 
-  AsyncElegantOTA.begin(&server);    
+
 
   // Lancer le serveur
   server.begin();
